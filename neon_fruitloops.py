642946f2d5f89713447ad5f83255beeffc64beee
bambivalente <33291780+bambivalente@users.noreply.github.com>
2017-11-04 22:36:55 +0100

    Add files via upload

---
+++ neon_fruitloops.py
@@ -0,0 +1,6 @@
+start = 999
+while start:
+    print(" {} neon fruitloops in the bowl, {} neon fruitloops.".format(start, start))
+    print("Put one on your spoon and stuff it in your pie hole.")
+    start -= 1
+    print(" {} neon fruitloops in the bowl.".format(start))
